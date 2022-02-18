d71763429960f4542a3f9bb038b334fdf4fbfdf3
Theo <67963370+theoladas@users.noreply.github.com>
2022-02-18 14:54:43 +0000

    Added - JS functionality in burger nav

---
+++ Interior Consultant/app.js
@@ -0,0 +1,12 @@
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const logo = document.querySelector("#logo");
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("navActive");
+    logo.classList.toggle("remove-logo");
+    burger.classList.toggle("toggle");
+    burger.style.marginLeft = "auto";
+  });
+};
+navSlide();

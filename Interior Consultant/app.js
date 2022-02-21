cc536f88ce1c1236e935b517aa8b0b45c6b52bd7
Theo <theodorosladasmail@gmail.com>
2022-02-21 10:52:44 +0000

    Fixed - card size responsive issues

--- Interior Consultant/app.js
+++ Interior Consultant/app.js
@@ -2,6 +2,7 @@ const navSlide = () => {
   const burger = document.querySelector(".burger");
   const nav = document.querySelector(".nav-links");
   const logo = document.querySelector("#logo");
+
   burger.addEventListener("click", () => {
     nav.classList.toggle("navActive");
     logo.classList.toggle("remove-logo");

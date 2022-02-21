42cce2e3081ba3dc4e2939608276e81c69802cf4
Theo <theodorosladasmail@gmail.com>
2022-02-21 13:40:26 +0000

    Added - overflow hidden when you click on burger

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const navSlide = () => {
   const burger = document.querySelector(".burger");
   const nav = document.querySelector(".nav-links");
   const logo = document.querySelector("#logo");
+  const body = document.querySelector("body");
 
   burger.addEventListener("click", () => {
     nav.classList.toggle("active");
     logo.classList.toggle("remove-logo");
     burger.classList.toggle("toggle");
     burger.style.marginLeft = "auto";
+    body.classList.toggle("overflow");
   });
 };
 navSlide();
